93dee646a09076298693c78d11c03c6bfd3dc0cd
Avinash Gardas <avinash@gmail.com>
2024-09-11 23:23:53 -0400

    init

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import tinyFishSVG from './images/icons8-fish-96.svg'
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
+          src={tinyFishSVG}
           alt="Next.js logo"
           width={180}
           height={38}
